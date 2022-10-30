4791678c7581a904262a188e554a82b77beab4de
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2022-10-30 02:39:12 +0200

    30/10/2022

--- src/Pet.js
+++ src/Pet.js
@@ -1,19 +1,11 @@
-const Pet = props => {
-    return(
+const Pet = (props) => {
+    return (
         <div>
             <h1>{props.name}</h1>
-            <h1>{props.animal}</h1>
-            <h1>{props.breed}</h1>
+            <h2>{props.animal}</h2>
+            <h2>{props.breed}</h2>
         </div>
     );
-}
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//         React.createElement("h2", {}, props.breed),
-//     ]);
-// };
+};
 
 export default Pet;
